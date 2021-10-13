c64b517b7fe33cbe7bac7405f7a90bfd960cd010
apopelinov <48691481+apopelinov@users.noreply.github.com>
2021-10-13 11:23:46 +0300

    fixed router warning

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -3,14 +3,15 @@ import { View, Text,Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/Home';
-import { createStackNavigator } from '@react-navigation/stack';
+import ProfileScreen from './src/screens/Profile';
 
-interface Props {
-  // any props that come into the component
-}
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+import { createStackNavigator } from '@react-navigation/stack';
+type RootStackParamList = {
+  Home: undefined, // undefined because you aren't passing any params to the home screen
+  Profile: { name: string }; 
 };
+
+
 
 const Stack = createNativeStackNavigator();
 
