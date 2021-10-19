64b6841e219865e59b7d263a1267061208300be3
apopelinov <48691481+apopelinov@users.noreply.github.com>
2021-10-19 12:01:42 +0300

    added wether api

--- AwesomeTSProject/src/screens/Profile.tsx
+++ AwesomeTSProject/src/screens/Profile.tsx
@@ -14,8 +14,40 @@ type Props = {
   navigation: ProfileScreenNavigationProp;
   route:any;
 };
-
+const getMovies = async () => {
+  try {
+   const response = await fetch('https://reactnative.dev/movies.json');
+   const json = await response.json();
+   console.log(json);
+  //  setData(json.movies);
+ } catch (error) {
+   console.error(error);
+ } finally {
+  //  setLoading(false);
+ }
+}
+const getWeather = async () => {
+  try {
+   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=b4b675963f07c6637d013a1e5edc1066');
+   const json = await response.json();
+   console.log(json);
+  //  setData(json.movies);
+ } catch (error) {
+   console.error(error);
+ } finally {
+  //  setLoading(false);
+ }
+}
 const ProfileScreen = ({ navigation, route }: Props) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+  return (
+  
+    <Button
+    onPress={() => {
+      getWeather();
+    }}
+    title="Press Me"
+  />
+  
+  )
 };
 export default ProfileScreen;
