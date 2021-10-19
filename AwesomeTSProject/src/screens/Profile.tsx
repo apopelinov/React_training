1f66159af0abc002b04f43577b92cadaae497c5a
apopelinov <48691481+apopelinov@users.noreply.github.com>
2021-10-19 14:55:45 +0300

    added location and wether with location

--- AwesomeTSProject/src/screens/Profile.tsx
+++ AwesomeTSProject/src/screens/Profile.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Button,Text } from 'react-native';
+import React, { useState } from 'react';
+import { Button,Text, View } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import Geolocation from 'react-native-geolocation-service';
+
 type RootStackParamList = {
   Home: undefined, // undefined because you aren't passing any params to the home screen
   Profile: { name: string }; 
@@ -9,11 +11,16 @@ type ProfileScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'Home'
 >;
-
+type Position = {
+  latitude:string;
+  longitude:string;
+  city:string;
+};
 type Props = {
   navigation: ProfileScreenNavigationProp;
   route:any;
 };
+
 const getMovies = async () => {
   try {
    const response = await fetch('https://reactnative.dev/movies.json');
@@ -26,12 +33,38 @@ const getMovies = async () => {
   //  setLoading(false);
  }
 }
-const getWeather = async () => {
+const getWeatherWithCoord = async (location:Position) => {
   try {
-   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=b4b675963f07c6637d013a1e5edc1066');
+   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat='+location.latitude+'&lon='+location.longitude+'&appid=b4b675963f07c6637d013a1e5edc1066');
    const json = await response.json();
    console.log(json);
-  //  setData(json.movies);
+   console.log(location);
+   location.city=json.name;
+ } catch (error) {
+   console.error(error);
+ } finally {
+ }
+}
+const getLocation = async (location:Position) => {
+  try {
+    Geolocation.getCurrentPosition((position) => { 
+      console.log(position); 
+      
+      location.latitude=position.coords.latitude.toString();
+      location.longitude=position.coords.longitude.toString();
+      console.log(location); 
+
+  }, (error) => { 
+      console.log(error.code, error.message); 
+      if (error.code==1)
+      {
+        Geolocation.requestAuthorization("whenInUse")
+      }
+  }, { 
+      enableHighAccuracy: true, 
+      timeout: 10000, 
+      maximumAge: 10000 
+  }); 
  } catch (error) {
    console.error(error);
  } finally {
@@ -39,15 +72,24 @@ const getWeather = async () => {
  }
 }
 const ProfileScreen = ({ navigation, route }: Props) => {
+  const [poss] = useState<Position>({latitude:'',longitude:'',city:'' });
   return (
-  
+    <View>
+
     <Button
     onPress={() => {
-      getWeather();
+      getWeatherWithCoord(poss);
     }}
-    title="Press Me"
+    title="get weather"
   />
-  
+  <Button
+  onPress={() => {
+    getLocation(poss);
+  }}
+  title="get location"
+/>
+<Text>This is {poss.city}'s profile</Text>
+</View>
   )
 };
 export default ProfileScreen;
